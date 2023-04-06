3a7a05f03ee125bd6def1f9f39c65074aba2a239
Miss Maryam <maryamdev.lop@gmail.com>
2023-04-06 22:18:11 +0500

    practice day 1

---
+++ script/app.js
@@ -0,0 +1,15 @@
+// day 1 
+
+// alert("As Salam O Alikum");
+
+var name = "MARYAM";
+// name =" ace";
+console.log(name);
+
+var nationality;
+nationality = "PAKISTANI"
+
+console.log(nationality);
+
+var floogle = "MARYAM";
+print (floogle);
