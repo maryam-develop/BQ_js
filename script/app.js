712e994249e462fa1e5b6f61ac0ec9790d0aeb6e
Miss Maryam <maryamdev.lop@gmail.com>
2023-04-07 12:45:46 +0500

    class work practice

--- script/app.js
+++ script/app.js
@@ -1,15 +1,52 @@
-// day 1 
+// Day 1a 6 April(class practice):
 
-// alert("As Salam O Alikum");
+//ALERT
 
-var name = "MARYAM";
-// name =" ace";
-console.log(name);
+// alert("Assalam 0 Alikum " )
 
-var nationality;
-nationality = "PAKISTANI"
+//PROMPT 
 
-console.log(nationality);
+//  var userName = prompt("your good name please" , "Maryam" , "anwer");
 
-var floogle = "MARYAM";
-print (floogle);
+//CONSOLE
+
+//  var user = "Maryam";
+//  var fuser = "Anwer";
+//  console.log("Hi " + user+ " " + fuser);
+
+
+// var sawera rasheed; SYTAX ERROR : MISSING
+
+//  how to make variable name
+
+// saweraRashid camelCase probably use
+// SaweraRashid PascalCase
+// sawera_rashid snake_case
+
+//MATHEMATICAL EXPRESSION
+
+// console.log(2+5); console.log(5-1); console.log(3*3); console.log(5/2);
+
+// // ** POWER 
+// console.log(2**5);
+
+// var EXPRESSION = (8 + (7 / 4) - (10 / 7) * (4 - 8)) + 4;
+// console.log(EXPRESSION);
+
+var age = + prompt("Your Age Please");
+var fage = + prompt("Your Father Age Please");
+
+// alert(age + fage);
+
+alert(Number(age)+ Number(fage));
+//alert(Number(age));
+
+console.log(typeof age);
+
+
+
+
+
+
+
+
