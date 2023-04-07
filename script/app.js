f975cc698dfa85455edce2808060c3ae866fe26c
Miss Maryam <maryamdev.lop@gmail.com>
2023-04-07 13:54:49 +0500

    practice 1 to 9 chapter

--- script/app.js
+++ script/app.js
@@ -33,20 +33,126 @@
 // var EXPRESSION = (8 + (7 / 4) - (10 / 7) * (4 - 8)) + 4;
 // console.log(EXPRESSION);
 
-var age = + prompt("Your Age Please");
-var fage = + prompt("Your Father Age Please");
+// var age = + prompt("Your Age Please");
+// var fage = + prompt("Your Father Age Please");
 
 // alert(age + fage);
 
-alert(Number(age)+ Number(fage));
-//alert(Number(age));
+// alert(Number(age)+ Number(fage));
+// alert(Number(age));
 
-console.log(typeof age);
-
-
+// console.log(typeof age);
 
 
 
 
+// Day 1b 6 April:
 
+
+// alert ( "hi" );
+// prompt ("your email","mary@gmail.com")
+// prompt ("your email"," ");
+// alert("user")
+
+// var ace =["welcome","hi"]
+
+// document.write("ace")
+
+// console.log("ace");
+// var name = "MarY";
+
+// document.write(name);
+
+
+// var weight = 150;
+// weight + 25;
+// var weight = weight + 25;
+
+// console.log(weight);
+
+// var originalNum = 23;
+// var nymToBeAdded = 15;
+// var newNum = originalNum  + nymToBeAdded;
+
+// console.log(newNum);
+
+// var originalNum = 90;
+//  originalNum = originalNum + 10;
+
+//  console.log(originalNum);
+
+//  var originalNum = "90";
+//  var newNum  = originalNum + 7;
+
+//  console.log(newNum);
+
+// var 1stPresident = "Washington"; illegal 
+// var prezWhoCame1st = "Washington"; legal
+// alert(144);
+
+
+//  var caseQty = 144;
+//  alert(caseQty);
+
+//  var popularNumber = 4;
+
+//  console.log(popularNumber);
+//  var popularNumber = 2 + 2;
+//  console.log(popularNumber);
+
+//  alert(2 + 2);
+
+
+// var popularNumber = 12 - 24;
+// console.log(popularNumber); it showa Numbers
+// console.log("popularNumber"); it shows string b/c inverted commas
+
+// var whatsLeftOver = 100 % 13;
+
+// console.log(whatsLeftOver);
+
+// var num = 1;
+//  var newNum = num++;
+// console.log(newNum);
+
+// var num = 1;
+//  var newNum = ++num;
+// console.log(newNum);
+
+
+// var num = 1;
+//  var newNum = --num;
+// console.log(newNum);
+
+// var num = 1;
+//  var newNum = num--;
+// console.log(newNum);
+
+// var message = "Thanks, ";
+// // var userName = "mary";
+// var banger = "!";
+// var show = message + userName + banger;
+// var show = message + "maryam" + banger;
+
+// alert(show);
+
+// alert("2" + "2");
+// alert("2 plus 2 equals " + 2 + 2);
+
+// var spec = prompt("Your species?", "human");
+
+// var question = "Your species?";
+// var defaultAnswer = "human";
+// var spec = prompt(question, defaultAnswer);
+
+// var numberOfCats = prompt("How many cats? ");
+// var tooManyCats = numberOfCats + 1;
+
+// console.log(tooManyCats);
+
+
+//  var numberOfCats = window.prompt("How many cats? ");
+//  var tooManyCats = numberOfCats + 1;
+
+//  console.log(tooManyCats);
 
